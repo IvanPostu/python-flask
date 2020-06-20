847da767101d4f10feda9255bd75354c658e5258
IvanPostu <ipostu20000127@gmail.com>
2020-06-21 00:35:08 +0300

    Added sqlite.

--- web-application/main.py
+++ web-application/main.py
@@ -1,13 +1,35 @@
-from collections import namedtuple
-from typing import List
+# from collections import namedtuple
+# from typing import List
 
 from flask import Flask, render_template, redirect, url_for, request
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db: SQLAlchemy = SQLAlchemy(app)
 
-Message = namedtuple('Message', ['text', 'tag'])
-messages: List[Message] = []
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(1024), nullable=False)
+
+    def __init__(self, text, tags):
+        self.text = text
+        self.tags = [
+            Tag(text=tag.strip()) for tag in tags.split(',')
+        ]
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(32), nullable=False)
+
+    message_id = db.Column(db.Integer, db.ForeignKey(
+        'message.id'), nullable=False)
+    message = db.relationship('Message', backref=db.backref('tags', lazy=True))
+
+
+db.create_all()
 
 
 @app.route('/', methods=['GET'])
@@ -17,12 +39,14 @@ def index():
 
 @app.route('/main', methods=['GET'])
 def main():
-    return render_template('main.html', messages=messages)
+    return render_template('main.html', messages=Message.query.all())
 
 
 @app.route('/add_message', methods=['POST'])
 def add_message():
     text = request.form['text']
     tag = request.form['tag']
-    messages.append(Message(text, tag))
+    db.session.add(Message(text, tag))
+    db.session.commit()
+
     return redirect(url_for('main'))
