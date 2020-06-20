18c0c191c9052b7837dd731eb17122208f429c2d
IvanPostu <ipostu20000127@gmail.com>
2020-06-20 23:17:46 +0300

    Save messages in memory and show it to template.

--- web-application/main.py
+++ web-application/main.py
@@ -1,14 +1,28 @@
-from flask import Flask, render_template
+from collections import namedtuple
+from typing import List
+
+from flask import Flask, render_template, redirect, url_for, request
 
 
 app = Flask(__name__)
 
+Message = namedtuple('Message', ['text', 'tag'])
+messages: List[Message] = []
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
 
-@app.route('/main')
+@app.route('/main', methods=['GET'])
 def main():
-    return 'main'
+    return render_template('main.html', messages=messages)
+
+
+@app.route('/add_message', methods=['POST'])
+def add_message():
+    text = request.form['text']
+    tag = request.form['tag']
+    messages.append(Message(text, tag))
+    return redirect(url_for('main'))
